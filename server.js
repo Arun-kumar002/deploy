2978bcd58db2ac5a74d72bf6a995570407bf2665
Arun-kumar002 <arunsubramanian002@gmail.com>
2023-02-27 19:58:21 +0530

    ajkslkl

---
+++ server.js
@@ -0,0 +1,81 @@
+const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+mongoose.set("strictQuery", false);
+
+const app = express();
+app.use((req, res, next) => {
+  console.log("im");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  app.options("*", (req, res) => {
+    // allowed XHR methods
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, PATCH, PUT, POST, DELETE, OPTIONS"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.send();
+  });
+  next();
+});
+
+//middlewares
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://monumental-kulfi-a4839a.netlify.app",
+    ],
+    credentials: true,
+  })
+);
+app.options("*", cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+//Routes middleware
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
+
+//Error Middleware
+app.use(errorHandler);
+
+//Database connection
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(5000, () => {
+      console.log(`server running on port${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
